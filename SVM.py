2e06aae610d14739fe1360f075dc64d063a5b59a
Stephen Wen <a5875246a@gmail.com>
2021-04-29 11:05:54 +0800

    Update SVM.py

--- SVM.py
+++ SVM.py
@@ -97,23 +97,6 @@ y_xor = np.logical_xor(X_xor[:, 0] > 0, #取二維陣列的第一維度大於零
                        X_xor[:, 1] > 0)
 y_xor = np.where(y_xor, 1, -1)
 
-plt.scatter(X_xor[y_xor == 1, 0],
-            X_xor[y_xor == 1, 1],
-            c='b', marker='x',
-            label='1')
-plt.scatter(X_xor[y_xor == -1, 0],
-            X_xor[y_xor == -1, 1],
-            c='r',
-            marker='s',
-            label='-1')
-
-plt.xlim([-3, 3])
-plt.ylim([-3, 3])
-plt.legend(loc='best')
-plt.tight_layout()
-#plt.savefig('images/03_12.png', dpi=300)
-plt.show()
-
 
 '''
 SVM實作於此資料上
